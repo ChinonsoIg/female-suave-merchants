d6ae7dc69238bb62ba3bde774aac46c7e85dd391
ChinonsoIg <ojononso@yahoo.com>
2023-01-01 19:29:53 +0100

    make layout mobile responsive

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,18 +1,40 @@
-import React from 'react'
-import Navbar from './nav';
+import React from "react";
+import Navbar from "./nav";
 import styles from "../../styles/Layout.module.scss";
-import Footer from './footer';
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <Navbar />
-      <main className={styles.main}>
-        <div className={styles.mainBox}>{children}</div>
-      </main>
-      <Footer />
-    </>
-  )
-}
+    <div className={styles.view}>
+      <div className={styles.sidebar}>
+        <img
+          src="https://picsum.photos/id/237/200/200"
+          width="100"
+          height="100"
+          alt="avatar"
+          className={styles.avatar}
+        />
+        <div>
+          <p>John Doe</p>
+          <p>john@yahoo.com</p>
+          <p>Date</p>
+        </div>
+        <button>Edit profule</button>
+      </div>
+
+      
+      <div className={styles.main}>
+        <Navbar />
+        <div className={styles.mainBox}>
+          {/* <section className={styles.contents}> */}
+            {children}
+            
+          {/* </section> */}
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,98 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import axios from "axios";
 import { Inter } from "@next/font/google";
 
-import Layout from "./components/layout"
+import Layout from "./components/layout";
 import styles from "./../styles/Home.module.scss";
+import Image from "next/image";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API;
 
 const inter = Inter({ subsets: ["latin"] });
 
-
 export default function Component() {
-  const router = useRouter(); 
-  const { status, data: session } = useSession();
+  const router = useRouter();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const ses = useSession();
+  const getProducts = async () => {
+    // try {
+    //   setLoading(true);
+    //   const response = await axios({
+    //     method: "GET",
+    //     url: `${BASE_URL}/products`,
+    //     headers: {
+    //       "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWUxNzVmNWIzZmVjMWEyNWRiMDI4ZmYiLCJuYW1lIjoiSmFuZSBEb2UiLCJpYXQiOjE2NzI1Mjc3ODksImV4cCI6MTY3MjYxNDE4OX0.wpppcdtuVr7ERoMzyn5sOVY5mAZCh8QUlUlxUCAfzBw`,
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
+    //   console.log("res: ", response)
+    //   const { data } = response;
+    //   setProducts(data);
+    // } catch (error) {
+    //   setLoading(false);
+    //   console.log(error);
+    // }
+  };
 
-  // useEffect(() => {
-  //   if (status === "unauthenticated" ) router.replace("/auth/signin");
-  // }, [status])
+  useEffect(() => {
+    const fetchInactiveVehicles = async () => {
+      try {
+        const res = await fetch(`${BASE_URL}/products`, {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWUxNzVmNWIzZmVjMWEyNWRiMDI4ZmYiLCJuYW1lIjoiSmFuZSBEb2UiLCJpYXQiOjE2NzI1Mjc3ODksImV4cCI6MTY3MjYxNDE4OX0.wpppcdtuVr7ERoMzyn5sOVY5mAZCh8QUlUlxUCAfzBw`,
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
 
-  console.log("ses aftre login:", ses)
+        const data = await res.json();
+        setProducts(data);
+        console.log("data: ", data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  // if(status === "authenticated") {
-    return <Layout>
-      SIGNED IN AS {session?.user?.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
+    fetchInactiveVehicles();
+    getProducts();
+  }, []);
+
+  console.log("products:", products);
+
+  return (
+    <Layout>
+      <div className={styles.content}>
+        <section className={styles.products}>
+          <div className={styles.cards}>
+            <div>total sales</div>
+            <div>total returned</div>
+            <div>total amt</div>
+          </div>
+          <p>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<p>Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+<p>Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+<p>Where can I get some?
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+
+<p>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+<p>Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+<p>Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+<p>Where can I get some?
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+        </section>
+      </div>
     </Layout>
-  // }
-
-  // return <Layout>
-  //   NOT SIGNED IN <br/>
-  //   <button onClick={() => signIn()}>Sign in</button>
-  // </Layout>
+  );
 }
