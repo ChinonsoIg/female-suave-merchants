5abdaeada0b753676543227fee1e1a8472379751
ChinonsoIg <ojononso@yahoo.com>
2023-02-01 20:29:09 +0100

    use status detection to avoid page flicker

--- pages/add-product/index.js
+++ pages/add-product/index.js
@@ -1,34 +1,99 @@
-import { useEffect } from 'react'
+import styles from "../../styles/AddProduct.module.scss";
+import { useEffect, useState } from 'react'
 import { useSession } from "next-auth/react";
 import { useRouter } from 'next/router';
 import SharedLayout from '../../components/layout/SharedLayout';
+import { redirect } from "next/dist/server/api-utils";
 
-const Home = () => {
-  // const router = useRouter();
-  // const { status, data } = useSession();
-  // const ses = useSession();
+const AddProduct = () => {
+  const [checked, setChecked] = useState(false);
+  const [formInputs, setFormInputs] = useState({ });
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (status === "unauthenticated" ) router.replace("/auth/signin");
-  // }, [status])
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/auth/signin")
+    },
+  })
+
+  const handleInputsChange = (e) => {
+    setFormInputs(() => ({
+      ...formInputs,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let data = { ...formInputs, status: !checked ? "out of stock" : "available" }
+    console.log("ll: ", data);
+
+  }
   
-  // console.log("status homepage: ", ses);
-  // console.log("data: ", data)
+  const handleCheckbox = () => {
+    setChecked(!checked);
+  };
 
-  // if (status === "authenticated") {
-  //   return (
-  //     <SharedLayout>
-  //       <div>Add home</div>
-  //     </SharedLayout>
-  //   );
-  // }
+
+  if (status === "loading") {
+    return <h1>"Loading or not authenticated..."</h1>
+  }
 
   return (
     <SharedLayout>
-      <div>add product.</div>
+      <h1>Add new product</h1>
+      <form className={styles.form_container} onSubmit={handleSubmit}>
+        <div className={styles.first_box}>
+          <label htmlFor="name">
+            Name
+            <input type="text" name="name" placeholder="Name" onChange={handleInputsChange} />
+          </label>
+          <label htmlFor="category">Category
+            <select name="category" id="pet-select" onChange={handleInputsChange}>
+              <option value="">--Choose an option--</option>
+              <option value="dog">Dog</option>
+              <option value="cat">Cat</option>
+            </select>
+          </label>
+        </div>
+        <div className={styles.second_box}>
+          <label htmlFor="price">
+            Price
+            <input type="number" name="price" placeholder="Price" onChange={handleInputsChange} />
+          </label>
+          <label>
+            Quantity
+            <input type="number" name="quantity" placeholder="Quantity" onChange={handleInputsChange} />
+          </label>
+        </div>
+        <div className={styles.textarea}>
+          <label htmlFor="description">
+            Description
+            <textarea type="text" name="description" placeholder="Description" onChange={handleInputsChange} />
+          </label>
+        </div>
+        <div className={styles.last_box}>
+          <label htmlFor="status">
+            Status
+            <input
+            type="checkbox"
+            checked={checked}
+            onChange={handleCheckbox}
+            className={styles.switch}
+            />
+          </label>
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+      <p>{checked.toString()}</p>
     </SharedLayout>
   )
-  
+
 }
 
-export default Home
+export default AddProduct
--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,17 @@ export default function Component() {
   const [limit, setLimit] = useState(0);
   const [page, setPage] = useState(0);
   const [search, setSearch] = useState("");
-  const { data: session, status } = useSession();
-  // const router = useRouter();
+  const router = useRouter();
+
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/auth/signin")
+    },
+  })
 
   const allNums = printNums();
-
   const handleLimit = (event) => {
     const value = Number(event.target.value);
     setLimit(value);
@@ -38,11 +44,11 @@ export default function Component() {
     try {
       const res = await fetch(
         `${BASE_URL_LOCAL}/products?limit=${limit}&page=${page}&search=${search}`, {
-          headers: {
-            "Authorization": `Bearer ${session?.user?.token}`,
-            "Access-Control-Allow-Origin": "*"
-          }
+        headers: {
+          "Authorization": `Bearer ${session?.user?.token}`,
+          "Access-Control-Allow-Origin": "*"
         }
+      }
       );
 
       if (res.status !== 200) {
@@ -70,105 +76,106 @@ export default function Component() {
 
   useEffect(() => {
     fetchUsers()
-  }, [limit, session])
+  }, [limit])
 
   // console.log(" lim: ", limit);
   // console.log(" data: ", data)
 
+  if (status === "loading") {
+    return <h1>"Loading or not authenticated..."</h1>
+  }
 
-  if (session) {
-    return (
-      <SharedLayout>
-        <h1>Dashboard</h1>
-        <section className={styles.figures_grid_container}>
-          <div className={styles.figures_grid_child}>
-            <p className={styles.grid_title}>Sales</p>
-            <p className={styles.grid_number}>100</p>
-          </div>
-          <div className={styles.figures_grid_child}>
-            <p className={styles.grid_title}>Income</p>
-            <p className={styles.grid_number}>100</p>
-          </div>
-          <div className={styles.figures_grid_child}>
-            <p className={styles.grid_title}>Customers</p>
-            <p className={styles.grid_number}>100</p>
-          </div>
-          <div className={styles.figures_grid_child}>
-            <p className={styles.grid_title}>Products</p>
-            <p className={styles.grid_number}>100</p>
-          </div>
-        </section>
 
-        {/* TODO:  Bar/Pie chart */}
-        {/* <section>
+  return (
+    <SharedLayout>
+      <h1>Dashboard</h1>
+      <section className={styles.figures_grid_container}>
+        <div className={styles.figures_grid_child}>
+          <p className={styles.grid_title}>Sales</p>
+          <p className={styles.grid_number}>100</p>
+        </div>
+        <div className={styles.figures_grid_child}>
+          <p className={styles.grid_title}>Income</p>
+          <p className={styles.grid_number}>100</p>
+        </div>
+        <div className={styles.figures_grid_child}>
+          <p className={styles.grid_title}>Customers</p>
+          <p className={styles.grid_number}>100</p>
+        </div>
+        <div className={styles.figures_grid_child}>
+          <p className={styles.grid_title}>Products</p>
+          <p className={styles.grid_number}>100</p>
+        </div>
+      </section>
+
+      {/* TODO:  Bar/Pie chart */}
+      {/* <section>
           <h2>Bar/Pie chart</h2>
         </section> */}
 
-        <section className={styles.data_table}>
-          <div>
-            <h2>Table</h2>
-            <table className={styles.products_container}>
-              <tbody>
-                <tr>
-                  <th>S/N</th>
-                  <th>Name</th>
-                  <th>Category</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                  <th>Status</th>
-                </tr>
-                {
-                  data.products && data.products.map((item, index) => (
-                    <tr>
-                      <td>{index + 1}</td>
-                      <td>{item.name}</td>
-                      <td>{item.category}</td>
-                      <td>{item.quatity}</td>
-                      <td>{item.price}</td>
-                      <td>{item.status}</td>
-                      <td>
-                        {/* <span><BsThreeDotsVertical /></span> */}
-                        <span>More...</span>
-                      </td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
-          </div>
+      <section className={styles.data_table}>
+        <div>
+          <h2>Table</h2>
+          <table className={styles.products_container}>
+            <tbody>
+              <tr>
+                <th>S/N</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Status</th>
+              </tr>
+              {
+                data.products && data.products.map((item, index) => (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.category}</td>
+                    <td>{item.quatity}</td>
+                    <td>{item.price}</td>
+                    <td>{item.status}</td>
+                    <td>
+                      {/* <span><BsThreeDotsVertical /></span> */}
+                      <span>More...</span>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
 
-          <div className={styles.data_modifier}>
-            <p className={styles.items_per_page}>Showing
-              <select
-                className={styles.num_value}
-                value={data?.perPageCount} 
-                onChange={handleLimit}
-              >
-                {allNums.map((num, ind) => 
-                  <option key={ind}>{num}</option>
-                )}
-              </select> out of {data?.totalCount}
-              <MdOutlineKeyboardArrowDown onClick={handleLimit} size={20} color="#213F7D" className={styles.items_per_page_icon} />
-            </p>
-            <p>
-              Paginate
-              {/* <Pagination pageCount={pageCount} handlePageClick={handlePageClick} /> */}
-            </p>
-          </div>
-        </section>
+        <div className={styles.data_modifier}>
+          <p className={styles.items_per_page}>Showing
+            <select
+              className={styles.num_value}
+              value={data?.perPageCount}
+              onChange={handleLimit}
+            >
+              {allNums.map((num, ind) =>
+                <option key={ind}>{num}</option>
+              )}
+            </select> out of {data?.totalCount}
+            <MdOutlineKeyboardArrowDown onClick={handleLimit} size={20} color="#213F7D" className={styles.items_per_page_icon} />
+          </p>
+          <p>
+            Paginate
+            {/* <Pagination pageCount={pageCount} handlePageClick={handlePageClick} /> */}
+          </p>
+        </div>
+      </section>
 
 
 
-        <button
-          onClick={() => signOut()}
-        >Sign out</button>
-      </SharedLayout>
-    );
+      <button
+        onClick={() => signOut()}
+      >Sign out</button>
+    </SharedLayout>
+  );
 
-  }
 
-  console.log("acces decnied", session)
-  return <AccessDenied />
+
 
 }
 
